411041cce5fefa6ddef9c8f8c1c43d78fdcb6019
Harsh <harsh.manchanda.129@gmail.com>
2024-02-03 02:05:09 +0530

    Feat: Changes Added and Notes Folder Removed

---
+++ core/vege/seed.py
@@ -0,0 +1,46 @@
+# To create fake data for our db
+# We will use faker library of python
+
+from faker import Faker
+fake = Faker()
+import random
+from .models import *
+
+def create_subject_marks(n):
+    try:
+        student_obj = Student.objects.all()
+        for student in student_obj:
+            subjects = Subject.objects.all()
+            for subject in subjects:
+                SubjectMarks.objects.create(
+                    subject = subject,
+                    student = student,
+                    marks = random.randint(1,100)
+                )
+    except Exception as e:
+        print(e)
+
+def seed_db(n=10)->None:
+    try:
+        for _ in range(n):
+            department_obj = Department.objects.all()
+            random_index = random.randint(0,len(department_obj)-1)
+            department = department_obj[random_index]
+            student_id = f'Std-0{random.randint(100,999)}'
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(18,26)
+            student_address = fake.address()
+
+            student_id_obj = StudentID.objects.create(student_id = student_id)
+
+            student_obj = Student.objects.create(
+                department = department,
+                student_id = student_id_obj,
+                student_name = student_name,
+                student_email = student_email,
+                student_age = student_age,
+                student_address = student_address,
+            )
+    except Exception as e:
+        print(e)
